8d8086a101d2a5523441c23acaac5cb7c53d3c29
fahadkhan2000 <fahad.khan@mapcase.de>
2016-02-15 17:09:38 +0100

    Test Code Indented
    Test code made more cleaner..!!!

--- poly_regexp_test/poly_regexp_test.go
+++ poly_regexp_test/poly_regexp_test.go
@@ -1,16 +1,17 @@
 package poly_regexp_test
+
 import (
-"testing"
- "PolyEval_RegExp_GoLang/poly_regexp"
+	"PolyEval_RegExp_GoLang/poly_regexp"
+	"testing"
 )
 
 func TestCalculateFinalResult(t *testing.T) {
 	for key, value := range testMap {
 		var testMonArray = poly_regexp.CreateTermsByRegExp(key)
-		if(poly_regexp.CalculateFinalResult(testMonArray, testVal) != value) {
+		if poly_regexp.CalculateFinalResult(testMonArray, testVal) != value {
 			t.Error("Status: Error")
 			return
-		} 
+		}
 		if len(testMonArray) <= 0 {
 			t.Error("Status: Invalid Polynomial")
 			return
@@ -25,17 +26,17 @@ type testpair struct {
 }
 
 var testVal = 1.9
-var testMap = map[string]float64 {
-	"-x" : -1.9, 
-	"x^1" : 1.9, 
-	"x^11" : 1164.9025889821899,
-	"+x^12" : 2213.3149190661607,
-	"-2x^10" : -1226.2132515602, 
-	"+100" : 100.000, 
-	"-100.001" : -100.001, 
-	".2x^3 + .2x + .2" : 1.9517999999999998, 
-	"-2x^3-2x-2" : -19.517999999999997, 
-	"+1x^2 + 3x^3 + 5x^5 + 7x^7 +1.17" : 774.8721672999999, 
-	"1x^2+3x^3+5x^5+7x^7+1.17+0" : 774.8721672999999,
-	"- 40x^4 + 30x^3 + x^5 -20x^2 +10x+81.3" : -262.65301,
-} 
+var testMap = map[string]float64{
+	"-x":                                     -1.9,
+	"x^1":                                    1.9,
+	"x^11":                                   1164.9025889821899,
+	"+x^12":                                  2213.3149190661607,
+	"-2x^10":                                 -1226.2132515602,
+	"+100":                                   100.000,
+	"-100.001":                               -100.001,
+	".2x^3 + .2x + .2":                       1.9517999999999998,
+	"-2x^3-2x-2":                             -19.517999999999997,
+	"+1x^2 + 3x^3 + 5x^5 + 7x^7 +1.17":       774.8721672999999,
+	"1x^2+3x^3+5x^5+7x^7+1.17+0":             774.8721672999999,
+	"- 40x^4 + 30x^3 + x^5 -20x^2 +10x+81.3": -262.65301,
+}
